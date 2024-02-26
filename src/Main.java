1b14a5ee00d6c9954771468bad029858a84da9a3
Alexander7145 <aberofeevy@gmail.com>
2024-02-26 19:27:51 +0300

    Массивы. Часть 2.

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,107 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Main {
+public class Main {//
+
     public static void main(String[] args) {
-        int[] weight = new int[3];
-        weight[0] = 1;
-        weight[1] = 2;
-        weight[2] = 3;
-        {System.out.println();}
-        double[] numbers = {1.57, 7.654, 9.986};
-        {System.out.println();}
-        int[] dayTemperature = {3, 5, 7, 8, 6, 4};
-        System.out.println();
-
-        int sugarWeight = weight[0];
-        {System.out.print(" " + weight[0] + ",");
-            System.out.print(" " + weight[1] + ",");
-            System.out.print(" " + weight[2]);}
-
-        System.out.println();
-        for (int i = 0; i < 3; i++) {
-            System.out.print(numbers[i]);
-            if (i != numbers.length - 1)
-                System.out.print(", ");}
-        System.out.println();
-
-        for (int a = 0; a < 6; a++) {
-            System.out.print(" " + dayTemperature[a]);
-            if (a != dayTemperature.length - 1)
-                System.out.print(", ");}
-        {System.out.println();}
-        {System.out.print(" " + weight[2] + ",");
-            System.out.print(" " + weight[1] + ",");
-            System.out.print(" " + weight[0]);}
-        {System.out.println();}
-        for (int i = 2; i >= 0; i--) {
-            System.out.print(numbers[i]);
-            if (i != numbers.length - 3)
-                System.out.print(", ");}
-        {System.out.println();}
-
-        for (int a = 5; a >= 0; a--) {
-            System.out.print(" " + dayTemperature[a]);
-            if (a != dayTemperature.length - 6)
-                System.out.print(", ");}
-
-        {System.out.println();}
-        for (int a = 0; a < 6; a++)
-            if (dayTemperature[a] % 2 != 0)
-                dayTemperature[a] = dayTemperature[a] + 1;
-
-        {System.out.print(Arrays.toString(dayTemperature));}
+        ;//
+        task1();
+        task2();
+        task3();
+        task4();
     }
 
 
+    public static int[] generateRandomArray() {//
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+
+    public static void task1() {
+        System.out.println(" Задача 1 ");
+        int[] arr = generateRandomArray();
+        double sum = 0.0;
+        for (int index = 0; index < arr.length; index++) {
+            sum += arr[index];
+        }
+        System.out.println(" Сумма трат за месяц составила " + sum + " рублей ");
+
+
+    }
+
+    public static void task2() {
+        System.out.println(" Задача 2 ");
+        int[] payment = generateRandomArray();
+        int min = payment[0];
+        for (int i = 0; i < payment.length; i++) {
+            if (min > payment[i])
+                min = payment[i];
+        }
+
+        int max = 0;
+        for (int i = 0; i < payment.length; i++)
+            if (max < payment[i]) {
+                max = payment[i];
+            }
+        System.out.println(" Максимальная сумма трат за месяц составила " + max + " рублей . Минимальная сумма трат за месяц составила " + min + " рублей ");
+
+
+    }
+
+
+    public static void task3() {
+        System.out.println(" Задача 3 ");
+        int[] payment = generateRandomArray();
+        double sum = 0;
+        for (int i = 0; i < payment.length; i++) {
+            sum += payment[i];
+        }
+        double average = sum / payment.length;
+        System.out.println(" Средняя сумма трат за месяц = " + average + " рублей ");
+
+    }
+
+    public static void task4() {
+        System.out.println(" Задача 4 ");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--)
+            System.out.print(reverseFullName[i]);
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
